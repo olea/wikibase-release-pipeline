5630d12e2f14f63408efaee743778edac2c094c0
Ismael Olea <ismael@olea.org>
2025-06-11 18:22:31 +0200

    This way the configuration for WikibaseManifest is read, but still not sure if it's a proper practice.
    See https://github.com/olea/wikibase-release-pipeline/issues/2

--- deploy/config/Extensions.php
+++ deploy/config/Extensions.php
@@ -83,3 +83,20 @@ wfLoadExtension( 'extensions/WikibaseLexemeCirrusSearch' );
 
 // PropertySuggester
 wfLoadExtension( 'extensions/PropertySuggester' );
+
+
+// Extension:WikibaseManifest things
+
+// this should use external variables for the services names,
+// but it's a job for me from the future.
+$wgWbManifestExternalServiceMapping = [
+        // WDQS_PUBLIC_HOST
+	'queryservice_ui' => 'https://wbqs.local',
+        # queryservice is derived from Wikibase config if left out:
+	'queryservice' => 'https://wbqs.local/sparql',
+	// QUICKSTATEMENTS_PUBLIC_URL
+	'quickstatements' => 'https://wb.local/tools/quickstatements',
+	// OPENREFINE_VERSION ?
+	// RECONCILE_PORT=8000
+	'openrefine_reconcile' => 'https://wbqs.local:8000/${lang}/api',
+];
